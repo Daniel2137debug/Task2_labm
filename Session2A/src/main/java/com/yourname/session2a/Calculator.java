b0245fb9b0983b0771af4f1bf479bfb80b5810c7
Kr3cik2137 <moskalewicz2003@wp.pl>
2025-10-29 13:18:54 +0100

    Add files via upload

---
+++ Session2A/src/main/java/com/yourname/session2a/Calculator.java
@@ -0,0 +1,8 @@
+﻿package com.yourname.session2a;
+
+public class Calculator {
+    public int sum(int a, int b) { return a + b; }
+    public int subtract(int a, int b) { return a - b; }
+    public int multiply(int a, int b) { return a * b; }
+    public int divide(int a, int b) { return b == 0 ? -1 : a / b; }
+}
---
+++ Session2A/src/test/java/com/yourname/session2a/CalculadoraTest.java
@@ -0,0 +1,24 @@
+﻿package com.yourname.session2a;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    void testSum() { assertEquals(5, calc.sum(2,3)); }
+
+    @Test
+    void testSubtract() { assertEquals(1, calc.subtract(3,2)); }
+
+    @Test
+    void testMultiply() { assertEquals(6, calc.multiply(2,3)); }
+
+    @Test
+    void testDivide() { 
+        assertEquals(2, calc.divide(6,3)); 
+        assertEquals(-1, calc.divide(6,0)); 
+    }
+}
